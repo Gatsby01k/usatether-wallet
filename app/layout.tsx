d7f04e71579d457f7c367544af4b1f1d6cbc713c
Gatsby01k <vladyslavddos@gmail.com>
2025-10-30 16:22:33 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,27 @@
-
 import './globals.css';
 import type { Metadata } from 'next';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
+import Providers from './providers';
 
 export const metadata: Metadata = {
   title: 'USATether Wallet — Awwwards Style',
   description: 'American Web Wallet: Swap, Bridge, Buy, FAQ.',
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }){
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body>
-        <Navbar />
-        {children}
-        <Footer />
+        <Providers>
+          <Navbar />
+          {children}
+          <Footer />
+        </Providers>
       </body>
     </html>
   );
