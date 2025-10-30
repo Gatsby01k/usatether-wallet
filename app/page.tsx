c96a660b245c8e94f3f2265228f7d68240cfe445
Gatsby01k <vladyslavddos@gmail.com>
2025-10-30 15:57:33 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
-
 'use client';
+
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 import Starfield from '@/components/Starfield';
 import HeroExact from '@/components/HeroExact';
 import { HomeFeatures, FAQShort } from '@/components/HomeSections';
