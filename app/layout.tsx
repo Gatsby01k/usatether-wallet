023c3b1cd594ab643a989c8532e07130a6a822d6
Gatsby01k <vladyslavddos@gmail.com>
2025-10-31 10:42:03 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,11 +9,7 @@ export const metadata: Metadata = {
   description: 'American Web Wallet: Swap, Bridge, Buy, FAQ.',
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
